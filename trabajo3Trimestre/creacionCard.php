1f5ebfc13d0ed8b7fae61c95b0562a15f8d3b6ec
Azahara33 <azaharamartinez1@gmail.com>
2026-04-21 11:35:19 +0200

    actualizo creacionCard

--- trabajo3Trimestre/creacionCard.php
+++ trabajo3Trimestre/creacionCard.php
@@ -1,7 +1,32 @@
 <?php
 session_start();
 include 'conexion.php';
-
+if (isset($_REQUEST["nombre"])&&isset($_REQUEST["fecha"])&&isset($_REQUEST["descripcion"])&&isset($_REQUEST["capacidad"])){
+    if ($_REQUEST["nombre"]!== ""&&$_REQUEST["fecha"]!==""&&$_REQUEST["descripcion"]!==""&&$_REQUEST["capacidad"]!=="") {
+        $nombre=trim(strip_tags($_REQUEST["nombre"]));
+        $fecha=trim(strip_tags($_REQUEST["fecha"]));
+        $descripcion=trim(strip_tags($_REQUEST["descripcion"]));
+        $capacidad=trim(strip_tags($_REQUEST["capacidad"]));
+        $consulta="INSERT INTO eventos (nombre,fecha,descripcion,capacidad) VALUES(:nombre,:fecha, :descripcion,:capacidad)";
+        $resultado = $pdo-> prepare($consulta);
+        $ejecutado=$resultado -> execute(
+            [":nombre"=>$nombre, 
+            ":fecha" =>$fecha,
+            ":descripcion"=> $descripcion, 
+            ":capacidad"=> $capacidad
+            ]
+            );
+            if (!$ejecutado) {
+                echo "Error al insertar evento";
+            }else{
+                echo "EL evento se ha guardado correctamente";
+            }
+    }else{
+        echo "Hay campos vaciós";
+    }
+}else {  
+    echo "Faltan datos";
+}
 
 ?>
 <!DOCTYPE html>
@@ -11,13 +36,14 @@ include 'conexion.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modificación Card</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
 <body>
     <div class="con-principal .flex-column ">
         <div class="con-titulo ">
             <h1 style="text-align:center;">MODIFICAR DATOS EVENTO</h1>
         </div>
         <div class="con-formulario row justify-content-center" >
-            <form class="formulario col-md-6 max-auto border rounded" >
+            <form class="formulario col-md-6 max-auto border rounded" method="post" action="">
              <div class="mb-3">
                 <label for="nombre" class="form-label">Nombre</label>
                 <input type="text" name="nombre" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
